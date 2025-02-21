882999c29d89e920401e99055aa2bc52e7877fff
Swapnil Pande <swapnil.pande98@gmail.com>
2025-02-21 13:15:53 -0500

    Added wandb

--- first_train.py
+++ first_train.py
@@ -3,15 +3,42 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
+import wandb
 from air_hockey_challenge.framework import AirHockeyChallengeGymWrapper
+from wandb.integration.sb3 import WandbCallback
 
 
 def get_env():
     return AirHockeyChallengeGymWrapper("3dof-hit", interpolation_order=2)
 
 
+wandb.login()
+
+run = wandb.init(
+    # Set the project where this run will be logged
+    project="air-hockey",
+    # Track hyperparameters and run metadata
+    config={},
+    sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+    monitor_gym=True,  # auto-upload the videos of agents playing the game
+    save_code=True,  # optiona
+)
+
+
 # Parallel environments
 vec_env = make_vec_env(get_env, n_envs=12)
 
-model = PPO("MlpPolicy", vec_env, verbose=1, device="cpu")
-model.learn(total_timesteps=250000)
+model = PPO(
+    "MlpPolicy", vec_env, verbose=1, device="cpu", tensorboard_log=f"runs/{run.id}"
+)
+model.learn(
+    total_timesteps=250000,
+    callback=WandbCallback(
+        gradient_save_freq=100,
+        model_save_path=f"models/{run.id}",
+        verbose=2,
+    ),
+)
+
+
+run.finish()
